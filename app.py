5228d0f4c7b2d495b3956b769b59f0fd47208a83
ThermoDev <michael.pegios@hotmail.com>
2020-06-29 01:36:06 +1000

    Layout Changes

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/form')
+@app.route("/form")
 def form():
-    return render_template('form.html')
+    return render_template("form.html")
 
-if __name__ == '__main__':
-    app.debug = True
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        print("POST!")
+
+
+if __name__ == "__main__":
+    #app.debug = True
     app.run()
